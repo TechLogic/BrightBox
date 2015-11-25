45508bbc6a43e15ccb52fc8be399a9a487e750c7
Chris <chris_neu-mann@web.de>
2015-11-25 13:53:58 +0100

    SQL Query for create and insert
    SQL Query for create and insert

---
+++ database/Creation_File.sql
@@ -0,0 +1,29 @@
+create database if not exists Bright_Box_DB;
+
+CREATE TABLE if not exists `bright_box_db`.`bright_box` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL,
+  `identifier` VARCHAR(45) NULL,
+  UNIQUE INDEX `identifier_UNIQUE` (`identifier` ASC),
+  PRIMARY KEY (`id`));
+  
+CREATE TABLE if not exists `bright_box_db`.`sensor_data` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fk_bright_box` INT NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  `bloom` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `vega` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `grow` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `air_temperatur` DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+  `humidity` DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  `ph_value` DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+  `ec_value` DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+  `water_temperatur` DECIMAL(5,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id`),
+  INDEX `fk_bright_box_idx` (`fk_bright_box` ASC),
+  CONSTRAINT `fk_bb`
+    FOREIGN KEY (`fk_bright_box`)
+    REFERENCES `bright_box_db`.`bright_box` (`id`)
+    on update cascade
+    on delete cascade);
---
+++ database/Insert_File.sql
@@ -0,0 +1,2 @@
+insert into bright_box values(0,'Test','Test description','bb0000');
+insert into sensor_data values(0,1,'2015-11-25 13:38:30',1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8);
